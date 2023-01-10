9d895f1bae7b7abc27edcce62bb1b7f7e2741af4
Gautam Singh <goats.singh@gmail.com>
2023-01-10 19:38:09 +0530

    Update vectors assignment

--- 12.10.2.17/codes/perp.py
+++ 12.10.2.17/codes/perp.py
@@ -1,15 +1,20 @@
 import numpy as np
 
-def dot_prod(A,B,C):
+#Inner product definition
+#Returns inner product of
+#B-A and C-A
+def inner_prod(A,B,C):
     return (B-A).T@(C-A)
 
+#Create column vectors for points
 A = np.array([[3.0],[-4.0],[-4.0]])
 B = np.array([[2.0],[-1.0],[1.0]])
 C = np.array([[1.0],[-3.0],[-5.0]])
 
-p = dot_prod(A,B,C)
-q = dot_prod(B,C,A)
-r = dot_prod(C,A,B)
+#Take inner products one pair at a time
+p = inner_prod(A,B,C)
+q = inner_prod(B,C,A)
+r = inner_prod(C,A,B)
 
 #Check and print output
 print("Triangle ABC is", end=" ")
