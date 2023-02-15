06e9798fed687195034d61166a8908ed6016e3ec
Gautam Singh <goats.singh@gmail.com>
2023-02-15 18:55:50 +0530

    Code edit

--- 11.16.03.08/codes/binomial.c
+++ 11.16.03.08/codes/binomial.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 // Generate a bernoulli sample
 // p is the probability of success
 int bernoulli(double p) { 
     double s = (double)rand()/RAND_MAX;
     return (s <= p)?1:0;
+}
+
+// Calculate factorial of small numbers
+int fac(int n) {
+    int ans = 1;
+    for (int i = 1; i <= n; i++) ans *= i;
+    return ans;
 }
 
 int main() {
@@ -21,7 +29,10 @@ int main() {
         ++arr[ans];
     }
     // Verify with actual pmfs
-    double tharr[4] = {0.125, 0.375, 0.375, 0.125};
+    double tharr[n+1];
+    for (int i = 0; i <= n; i++) { 
+        tharr[i] = fac(n)/(fac(i)*fac(n-i))*pow(p,i)*pow(1-p,n-i);
+    }
     printf("Theoretical pmfs:\n");
     for (int i = 0; i <= n; i++) printf("%lf ", tharr[i]);
     printf("\nEmpirical pmfs:\n");
