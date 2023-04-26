cdb2c3e34e0c4c19e71a1ae363a72d5171c291d8
Gautam Singh <goats.singh@gmail.com>
2023-04-26 21:59:09 +0530

    Update learning model and presentation

--- pt100/esp32/codes/client/src/main.cpp
+++ pt100/esp32/codes/client/src/main.cpp
@@ -2,6 +2,7 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <LiquidCrystal.h>
 
 /**
  * This is a header file which contains WiFi network credentials and URLs
@@ -9,17 +10,26 @@
  *
  * const char *ssid = "YOUR-SSID-HERE";
  * const char *password = "YOUR-PASSWORD-HERE";
- * const char *tempURL = "URL-TO-PUT-TEMPERATURE"
+ * const char *tempURL = "URL-TO-PUT-TEMPERATURE";
 */
 #include "credentials.h"
 
-#define A -8.6837e-6
-#define B 3.245e-3
-#define C 1.553
-#define TEMP_PIN 34
+#define A -3.975e-6
+#define B 2.894e-3
+#define C 1.568
+#define PIN_TEMP 34
+#define RS 19
+#define EN 23
+#define D4 18
+#define D5 17
+#define D6 16
+#define D7 15
+#define NCOLS 16
+#define NROWS 2
 
 WiFiClient client;
 HTTPClient temp;
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 void setup() {
   Serial.begin(9600);
@@ -32,13 +42,21 @@ void setup() {
   Serial.println();
   Serial.println(WiFi.localIP());
   temp.begin(client, tempURL);
+  lcd.begin(NCOLS, NROWS);
+  lcd.clear(); lcd.setCursor(0,0);
+  lcd.print("Temperature ("); lcd.print((char)223); lcd.print("C)");
 }
 
 float getTemp() {
-  float analogValue = analogRead(TEMP_PIN);
+  float analogValue = analogRead(PIN_TEMP);
   float V = 3.3*analogValue/4095;
   Serial.println(V);
-  return (-B + sqrtl(B*B+4*A*(C-V)))/(2*A);
+  return (B - sqrtl(B*B+4*A*(C-V)))/(2*A);
+}
+
+void dispTemp(float t) { 
+  lcd.setCursor(0,1);
+  lcd.print(t);
 }
 
 void loop() {
@@ -48,5 +66,6 @@ void loop() {
   String requestBody;
   serializeJson(doc, requestBody);
   int httpResponseCode = temp.PUT(requestBody);
-  delay(2000);
+  dispTemp(doc["temp"]);
+  delay(2500);
 }
